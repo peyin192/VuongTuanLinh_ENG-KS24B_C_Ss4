a19a60df2475d4d8ff6e8f6b745e9038c61200fa
peyin192 <vuonglinh190206.qv@gmail.com>
2024-11-23 21:30:49 +0700

    upload ss04-1

---
+++ ex01.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	int number;
+	printf("Nhap vao 1 so nguyen: ");
+	scanf("%d",&number);
+	 if(number<0){
+	 	printf("Day la so am");
+	 }
+	 else{
+	 	printf("Day la so duong");
+	 }
+	 
+	
+}
