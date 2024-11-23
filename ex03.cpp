11680b646a864911d99b39ebc60e7974c7954909
peyin192 <vuonglinh190206.qv@gmail.com>
2024-11-23 22:03:06 +0700

    upload ss04-3

---
+++ ex03.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+	int number;
+	printf("Nhap vao 1 so nguyen: ");
+	scanf("%d",&number);
+	 if(number%3==0){
+	 	printf("Day la so chia het cho 3");
+	 }
+	 else if(number%5==0){
+	 
+	 
+	 	printf("Day la so chia het cho 5");
+	 }
+	 else if(number%3==0&&number%5==0){
+	 printf("Day la so chia het cho ca 3 va 5");
+ }
+	 else{
+	 	printf("SO nay khong chia het cho 3 va 5");
+	 }
+	 return 0;
+	 
+	
+}
