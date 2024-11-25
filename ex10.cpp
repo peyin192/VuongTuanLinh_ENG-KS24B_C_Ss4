005702dd73388aee9e5ec99f954026506a678f24
peyin192 <vuonglinh190206.qv@gmail.com>
2024-11-25 13:44:06 +0700

    Upload ex10.cpp

--- ex10.cpp
+++ ex10.cpp
@@ -17,19 +17,19 @@ int main() {
 	}
 	
 	else if(a>c&&a>b&&c>b){
-		printf("%d %d %d",a,c,b);
-	}
-	else if(b>a&&b>c&&a>c){
-		printf("%d %d %d",b,a,c);
-	}
-	else if(b>a&&b>c&&c>a){
 		printf("%d %d %d",b,c,a);
 	}
-	else if(c>a&&c>b&&a>b){
+	else if(b>a&&b>c&&a>c){
 		printf("%d %d %d",c,a,b);
 	}
+	else if(b>a&&b>c&&c>a){
+		printf("%d %d %d",a,c,b);
+	}
+	else if(c>a&&c>b&&a>b){
+		printf("%d %d %d",b,a,c);
+	}
 	else if(c>a&&c>b&&b>a){
-		printf("%d %d %d",c,b,a);
+		printf("%d %d %d",a,b,c);
 	}
 	else{
 		printf("Khong hop le");
